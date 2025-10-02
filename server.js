e0bc4324f96fe1695469838d13452ae8d9274b4e
Amisha Mulani <amishamulani0415@gmail.com>
2025-10-03 00:14:16 +0530

    initial backend setup

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+
+dotenv.config();
+const app = express();
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => res.send("API is running..."));
+app.use("/api/users", userRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
